728098dfa40e5e1d7a5caad91a276bd88510a958
Adam Wareing <adamwareing241@gmail.com>
2021-04-16 11:00:43 +1200

    Add about me and background slant

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -5,6 +5,7 @@ import Navigation from 'components/nav';
 
 import computer from 'assets/computer.png';
 import house from 'assets/house.png';
+import person from 'assets/person.png';
 
 export default class AppRoute extends React.Component {
 
@@ -45,6 +46,22 @@ export default class AppRoute extends React.Component {
           <div className='Header-content'>
             <div className='Grid u-flex'>
               {featureTiles}
+            </div>
+          </div>
+        </div>
+
+        <div className='Header-footer'></div>
+
+        <div className='AboutMe'>
+          <div className='Grid'>
+            <div className='u-width1of2'>
+              <img src={person} />
+            </div>
+            <div className='u-width1of2'>
+              <div className='AboutMe-content'>
+                <h2>Gidday, i’m adam!</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam imperdiet ligula vitae sapien pulvina. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam imperdiet ligula vitae sapien pulvina. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam imperdiet ligula vitae sapien pulvina.</p>
+              </div>
             </div>
           </div>
         </div>
