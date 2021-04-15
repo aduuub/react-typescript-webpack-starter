49065b3211e578cef75d18e17d301f05eba82f14
Adam Wareing <adamwareing241@gmail.com>
2021-04-16 11:12:49 +1200

    Add about me contact section

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -38,6 +38,7 @@ export default class AppRoute extends React.Component {
       <div>
         <Navigation />
 
+        {/* Header */}
         <div className='Header'>
           <div className='Header-title'>
             <h1>A statement about what you do</h1>
@@ -52,6 +53,7 @@ export default class AppRoute extends React.Component {
 
         <div className='Header-footer'></div>
 
+        {/* About me */}
         <div className='AboutMe'>
           <div className='Grid'>
             <div className='u-width1of2'>
@@ -63,6 +65,14 @@ export default class AppRoute extends React.Component {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam imperdiet ligula vitae sapien pulvina. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam imperdiet ligula vitae sapien pulvina. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam imperdiet ligula vitae sapien pulvina.</p>
               </div>
             </div>
+          </div>
+        </div>
+
+        <div className='HomeBlog'>
+          <div className='HomeBlog-header'>
+            <p className='HomeBlog-header-leading'>My thoughts on</p>
+            <h2 className='HomeBlog-header-main'>Project management</h2>
+            <p>I have a project management company that specialises in turning round renovations quickly. This is especially useful when you have tenants wanting to move in quickly, minimising rental loss, or when trading a property (buying to renovate and sell).<br /><br />Get in touch to get some assistance with your renovation. </p>
           </div>
         </div>
       </div>
