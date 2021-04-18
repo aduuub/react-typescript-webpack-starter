b4884d4079b45b6dfff2b6a63e318a1ae6e77f72
Adam Wareing <adamwareing241@gmail.com>
2021-04-19 11:41:22 +1200

    Responsive updates

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -7,30 +7,30 @@ export default class Navigation extends React.Component {
           <div className='Nav'>
             <div className='Grid'>
               {/* Left side */}
-              <div className='u-width3of4'>
+              <div className='u-md-width3of4'>
                 <ul className='Nav-links'>
                   {/* Name */}
                   <li className='Nav-links-item'>
                     <h3 className='Nav-title'>Adam Wareing</h3>
                   </li>
                   {/* Links */}
-                  <li className='Nav-links-item'>
+                  <li className='Nav-links-item Nav-links-item--tabletOnly'>
                     <button className='Button Button--bland'>About me</button>
                   </li>
-                  <li className='Nav-links-item'>
+                  <li className='Nav-links-item Nav-links-item--tabletOnly'>
                     <button className='Button Button--bland'>Property</button>
                   </li>
-                  <li className='Nav-links-item'>
+                  <li className='Nav-links-item Nav-links-item--tabletOnly'>
                     <button className='Button Button--bland'>Software development</button>
                   </li>
                 </ul>
               </div>
 
               {/* Right side */}
-              <div className='u-width1of4'>
+              <div className='u-md-width1of4'>
 
                 <ul className='Nav-links u-right'>
-                  <li className='Nav-links-item'>
+                  <li className='Nav-links-item Nav-links-item--tabletOnly'>
                     <button className='Button'>Get in touch</button>
                   </li>
                 </ul>
